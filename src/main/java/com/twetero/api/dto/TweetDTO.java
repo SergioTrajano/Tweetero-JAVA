d728cb83687d033309fabbd103b2581906ec90bc
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-22 18:51:40 -0300

    :sparkles: feat: create DTo for Tweets

---
+++ src/main/java/com/twetero/api/dto/TweetDTO.java
@@ -0,0 +1,12 @@
+package com.twetero.api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record TweetDTO(
+    @NotBlank
+    String username,
+
+    @NotBlank
+    String tweet
+) {
+}
