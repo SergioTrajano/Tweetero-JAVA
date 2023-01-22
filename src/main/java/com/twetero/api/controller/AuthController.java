a3cd09cb21aa78252d11e25af80f35faf14725d9
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-22 18:20:17 -0300

    :sparkles: feat: add controller for Auth

---
+++ src/main/java/com/twetero/api/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.twetero.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.twetero.api.dto.AuthDTO;
+import com.twetero.api.service.AuthService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("")
+public class AuthController {
+
+    @Autowired
+    private AuthService service;
+
+    @PostMapping("/sign-up")
+    public void signUp(@RequestBody @Valid AuthDTO authData) {
+        service.signUp(authData);
+    }
+}
