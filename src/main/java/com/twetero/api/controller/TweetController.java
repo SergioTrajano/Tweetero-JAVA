fc1a28c2f8ef878b8099553af407060574238620
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-22 21:56:52 -0300

    :sparkles: feat: create method to get tweets by page

--- src/main/java/com/twetero/api/controller/TweetController.java
+++ src/main/java/com/twetero/api/controller/TweetController.java
@@ -1,17 +1,23 @@
 package com.twetero.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.twetero.api.dto.TweetDTO;
+import com.twetero.api.model.Tweet;
 import com.twetero.api.service.TweetService;
 
 import jakarta.validation.Valid;
+import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/tweets")
 public class TweetController {
     
@@ -21,5 +27,11 @@ public class TweetController {
     @PostMapping
     public void post(@RequestBody @Valid TweetDTO tweetData) {
         service.post(tweetData);
+    }
+
+    @GetMapping
+    public List<Tweet> pagination(@RequestParam(defaultValue = "0") int page) {
+        System.out.println(page);
+        return service.findAll(page);
     }
 }
