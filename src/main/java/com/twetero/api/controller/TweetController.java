9e9e9939b7b004a82a51c48d663158db15bd73e2
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-01-23 00:29:04 -0300

    :sparkles: feat: implements method to get tweets by username

--- src/main/java/com/twetero/api/controller/TweetController.java
+++ src/main/java/com/twetero/api/controller/TweetController.java
@@ -3,6 +3,7 @@ package com.twetero.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,7 +32,11 @@ public class TweetController {
 
     @GetMapping
     public List<Tweet> pagination(@RequestParam(defaultValue = "0") int page) {
-        System.out.println(page);
         return service.findAll(page);
+    }
+
+    @GetMapping("/{username}")
+    public List<Tweet> getByUsername(@PathVariable String username) {
+        return service.findByUsername(username);
     }
 }
